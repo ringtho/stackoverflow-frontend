1579f61efa880c57969c402f841be46047d1fb84
Smith Ringtho <sringtho@gmail.com>
2023-11-19 20:21:55 +0300

    Refactor: Updated logout functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,14 @@
 import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './Navbar.scss'
 import { getAuthenticatedUser } from '../../api'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from '../../reduxSlice/usersSlice'
 
 const Navbar = () => {
-  const data = JSON.parse(localStorage.getItem('stackUser'))
-  const token = data?.token
   const { user } = useSelector(state => state.user)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const getUser = async () => {
@@ -22,6 +21,11 @@ const Navbar = () => {
   const username = user?.name?.split(' ')
   const initials = username && (username[0][0] + username[1][0])
 
+  const handleLogout = () => {
+    localStorage.removeItem('stackUser')
+    navigate('/login', { replace: true })
+  }
+
   return (
     <nav className="nav__container">
         <div className='logo'>
@@ -30,15 +34,10 @@ const Navbar = () => {
             </h3>
         </div>
         <div>
-            { !token 
-              ? <div>
-                  <Link to='/login'>Login</Link>
-                </div>
-              : <div>
-                  <div className='avatar'>{initials}</div>
-                  <Link to='/login'>Logout</Link>
-                </div>
-            }
+            <div>
+              <div className='avatar'>{initials}</div>
+              <p onClick={handleLogout} className='logout__btn'>Logout</p>
+            </div>
         </div>
     </nav>
   )
